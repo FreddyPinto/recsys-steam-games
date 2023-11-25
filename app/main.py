1799d5abb3598d50b64fd1d3b6b98c1eced7d8d3
FreddyPinto <freddypinto@outlook.com>
2023-11-24 21:36:42 -0500

    Add title, version and description

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routes import router
 
-app = FastAPI()
+app = FastAPI(
+    title="RecSys API for Steam Games",
+    version="0.2.0",
+    description="This API allows you to access data from Steam, a leading video game platform, and get personalized game recommendations based on a machine learning model. With this API, you can query information about game genres, users, developers and reviews, as well as get suggestions of similar or suitable games for you or other users."
+)
 
-app.include_router(router)
+app.include_router(router, tags=["Endpoints"])
