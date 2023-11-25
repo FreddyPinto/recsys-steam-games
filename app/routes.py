0ae8cdcd7ba002cfb4d95deb57f698e809e8335e
FreddyPinto <freddypinto@outlook.com>
2023-11-24 21:35:24 -0500

    Update routes

--- app/routes.py
+++ app/routes.py
@@ -1,74 +1,80 @@
-from fastapi import APIRouter, HTTPException, status
-from models import * 
-from data import df_endpoint1, df_endpoint2, df_endpoint3, df_endpoint4
+from fastapi import APIRouter, status, HTTPException, Path
+from database import *
 
 router = APIRouter()
 
-@router.get("/developer/{desarrollador}", response_description="Retorna cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora.",response_model=DeveloperResponse)
-def get_developer(desarrollador: str):
-    df_dev = df_endpoint1[df_endpoint1['developer'] == desarrollador].reset_index()
-    df_dev['is_free'] = df_dev['price'] == 0
-    result = df_dev.groupby('release_year').agg({'item_id': 'count', 'is_free': 'mean'})
-    result.columns = ['Cantidad de Items', 'Contenido Free']
-    result['Contenido Free'] = (result['Contenido Free'] * 100).round(2).astype(str) + '%'
-    result = result.reset_index().rename(columns={'release_year': 'Año'})
-    records = result.reset_index().to_dict('records')
-    
-    return DeveloperResponse(records=records)
+
+@router.get("/PlayTimeGenre/{genre}")
+async def play_time_genre(genre: str = Path(...,
+                                            description="Enter a genre",
+                                            example='Simulation')):
+    """
+        Returns the year with the most hours played for a given genre.
+    """
+    response = await PlayTimeGenre(genre)
+    if response is None:
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Genre {genre} not found")
+    return response
 
 
+@router.get("/UserForGenre/{genre}")
+async def user_for_genre(genre: str = Path(
+        description="Enter a genre",
+        example='RPG')):
+    """
+        Returns the user who accumulates the most hours played for a given genre.
+    """
+    response = await UserForGenre(genre)
+    if response is None:
 
-@router.get("/userdata/{User_id}",response_description="Retorna cantidad de dinero gastado por el usuario, el porcentaje de recomendación y cantidad de items.", response_model=UserdataResponse)
-def userdata(User_id: str):
-    df_user = df_endpoint2[df_endpoint2['user_id'] == User_id].reset_index()
-    dinero_gastado = df_user['total_spend'][0]
-    cantidad_items = df_user['items_count'][0]
-    porcentaje_recomendacion = (df_user[df_user['recommend'] == True]['recommend'].count()/cantidad_items * 100).round(2)
-
-    result = UserdataResponse(
-        Usuario=User_id,
-        Dinero_gastado=dinero_gastado,
-        Porcentaje_de_recomendación=f"{porcentaje_recomendacion}%",
-        Cantidad_de_items=cantidad_items
-    )
-
-    return result
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Genre {genre} not found")
+    return response
 
 
-@router.get("/UserForGenre/{genero}", response_description="Retorna el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año de lanzamiento.")
-def UserForGenre(genero: str):
-    df_genre = df_endpoint3[genero].reset_index()
-    user_max_hours = df_genre.groupby('user_id')[genero].sum().idxmax()
-    hours_by_year = df_genre[df_genre['user_id'] == user_max_hours].groupby('release_year')[genero].sum().reset_index()
-    hours_by_year.columns = ['Año', 'Horas']
-    hours_by_year['Horas'] = hours_by_year['Horas'].astype(int)
+@router.get("/UsersRecommend/{year}")
+async def users_recommend(year: int = Path(
+        description="Enter a year",
+        example=2013)):
+    """
+    Returns the top 3 MOST recommended games by users for the given year.
+    """
+    response = await UsersRecommend(year)
+    if response is None:
 
-    result = {
-        f"Usuario con más horas jugadas para Género {genero} " : user_max_hours,
-        "Horas jugadas": hours_by_year.to_dict('records')
-    }
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Year {year} not found")
+    return response
 
-    return result
 
-@router.get("/best_developer_year/{año}", response_description="Retorna el top 3 de desarrolladores con juegos MÁS recomendados por usuarios para el año dado.")
-def best_developer_year(year: int):
-    df_year = df_endpoint4[(df_endpoint4['posted_year'] == str(year)) & (df_endpoint4['recommend'] == True) & (df_endpoint4['sentiment_analysis'] == 2)]
-    top_developers = df_year['developer'].value_counts().nlargest(3).index.tolist()
+@router.get("/UsersWorstDeveloper/{year}")
+async def users_worst_developer(year: int = Path(
+        description="Enter a year",
+        example=2011)):
+    """
+    Returns the top 3 developers with the LEAST recommended games by users for the given year.
+    """
+    response = await UsersWorstDeveloper(year)
+    if response is None:
 
-    result = [{"Puesto 1" : top_developers[0]},
-              {"Puesto 2" : top_developers[1]},
-              {"Puesto 3" : top_developers[2]}]
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Year {year} not found")
+    return response
 
-    return result
 
-@router.get("/developer_reviews_analysis/{desarrollador}",response_description="Según el desarrollador, se retorna un diccionario con el nombre del desarrollador como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor positivo o negativo.", response_model=DeveloperReviewsAnalysisResponse)
-def developer_reviews_analysis(desarrollador: str):
-    df_dev = df_endpoint4[df_endpoint4['developer'] == desarrollador]
-    sentiment_counts = df_dev['sentiment_analysis'].value_counts()
-    negative_reviews = sentiment_counts.get(0, 0)
-    positive_reviews = sentiment_counts.get(1, 0) + sentiment_counts.get(2, 0)
+@router.get("/SentimentAnalysis/{developer}", response_description="Get the sentiment analysis by developer")
+async def sentiment_analysis(developer: str = Path(
+        description="Enter developer's name",
+        example="Ubisoft")):
+    """
+    Returns a developer with the total number of users reviews records categorized with a sentiment analysis.
+    """
 
-    reviews = ReviewAnalysis(Negativos=negative_reviews, Positivos=positive_reviews)
-    result = DeveloperReviewsAnalysisResponse(Desarrollador=desarrollador, Reviews=reviews)
+    response = await get_sentiment_by_developer(developer)
+    if response is None:
 
-    return result
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Developer {developer} not found")
+    return response
