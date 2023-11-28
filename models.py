2493f332ddd2c804955e0220da86af385fffb8ac
FreddyPinto <freddypinto@outlook.com>
2023-11-28 09:25:13 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 from pydantic import BaseModel
-from typing import Optional, Dict, List
+# from typing import Optional, Dict, List
 
 
-class SentimentCount(BaseModel):
-    Negative: int
-    Neutral: int
-    Positive: int
+# class SentimentCount(BaseModel):
+#     Negative: int
+#     Neutral: int
+#     Positive: int
 
 
-class SentimentAnalysis(BaseModel):
-    Dict[str, SentimentCount]
+# class SentimentAnalysis(BaseModel):
+#     Dict[str, SentimentCount]
 
 class Message(BaseModel):
     message: str
